c942fe85dd122de9e6bb70add480eaf132fcbc67
shj2022 <106890894+shj2022@users.noreply.github.com>
2022-07-17 10:48:43 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,15 @@
+let activity_level = 0
 activityRecognition.start()
-basic.forever(function () {
-    activityRecognition.show(activityRecognition.updateActivity())
-})
+let on = true
+for (let index = 0; index < 10; index++) {
+    if (activityRecognition.updateActivity() == "s") {
+        activity_level += 1
+    }
+    if (activityRecognition.updateActivity() == "w") {
+        activity_level += 2
+    }
+    if (activityRecognition.updateActivity() == "r") {
+        activity_level += 3
+    }
+}
+basic.showNumber(activity_level)
